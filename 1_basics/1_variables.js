4d268dbe24ec1660d4acb136fe7de589eff0d5cf
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-05-26 14:46:03 +0000

    another commit1

--- 1_basics/1_variables.js
+++ 1_basics/1_variables.js
@@ -1,26 +1,26 @@
-const accountID = 1234;
-// accountID =2348;  Throw error not allowed to change const
-// console.log(accountID);
+// const accountID = 1234;
+// // accountID =2348;  Throw error not allowed to change const
+// // console.log(accountID);
 
-// var a = "akshay";
-// {
-//     a = "chandel";
-// }
-// console.log(a);    // it means yeah scope of block nhi smjh paa rha h isliye hum isse use nhi krte 
+var a = "akshay";
+{
+    a = "chandel";
+}
+console.log(a);    // it means yeah scope of block nhi smjh paa rha h isliye hum isse use nhi krte 
 
-// let b = "hi";
-// console.log(b);
-// {
-//     b = "hello";
-//     b = "hii"
-// }
-// console.log(b)
+let b = "hi";
+console.log(b);
+{
+    b = "hello";
+    b = "hii"
+}
+console.log(b)
 
 // let is block scope and var is functional scope
-myName = "akshay";
-myName = "chandel"
-console.log(myName);
+// myName = "akshay";
+// myName = "chandel"
+// console.log(myName);
 
 // const rollNumber //cannot do that const should be declared
-let rollNumber;
-console.log(rollNumber); // undefined 
+// let rollNumber;
+// console.log(rollNumber); // undefined 
---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,13 @@
+// number 
+// bigint
+// string ="";
+// boolean
+// null => standalone value
+// undefined =>
+// symbol => uniquely identify
+// object
+
+let age = 5;
+let name = akshay;
+console.log(typeof(age));
+
