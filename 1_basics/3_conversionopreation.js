9bfe2309d2356b1a6120f7532ee534f03c9f92c8
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-05-26 15:18:10 +0000

    conversion Opreation

---
+++ 1_basics/3_conversionopreation.js
@@ -0,0 +1,48 @@
+// // let a = 33
+// let a = "33ab"
+// console.log(typeof a);
+
+// // changing string value in Number
+
+// let valueInNumber = Number(a); 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let b = null;
+// console.log(b);
+// let valueInNumber = Number(b); 
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// let c = undefined;
+// console.log(c);
+// let valueInNumber = Number(c); 
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(typeof c);
+
+// let d = true;
+// console.log(d);
+// let valueInNumber = Number(d); 
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(typeof d);
+
+let d = "akshay";
+console.log(d);
+let valueInNumber = Number(d); 
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(typeof d);
+  
+// 33=number
+// 33abc=NaN =value   and type still number
+
+let LoggedIn = 1;
+let conversonBoolean = Boolean(LoggedIn);
+console.log(conversonBoolean);
+console.log(typeof conversonBoolean);
+
+// "" => false
+// "akshay"=> true
