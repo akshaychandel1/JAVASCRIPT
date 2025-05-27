5569ea6597700eb245f7cc986c0c6307f0bee05c
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-05-27 06:21:07 +0000

    modified dataTypesSummary

--- 1_basics/4dataTypesSummary.js
+++ 1_basics/4dataTypesSummary.js
@@ -27,3 +27,37 @@ let myFunctionName = function () {
 // function myFuctionName (){
 // This is also a function
 // }
+
+
+// ***********  Memory Types ************
+
+// Stack , Heap
+// Stack is Used By all the primitive .by value so it gets copy of the orignal value
+// Heap is used By all the Non Primitive datatypes by using the refference 
+
+let myName = "akshay";
+let myotherName = "akku"
+
+console.log(myotherName);
+console.log(myName);
+
+
+// now with heap it is used by refference
+let user1 = {
+    eamil : "chandel@gmail.com",    
+}
+
+let user2 = user1
+
+user2.eamil = "akshay@gmail.com"
+console.log(user1);
+console.log(user2);
+
+
+// Output will be 
+// akku
+// akshay
+
+// value gets changed in nonprimitive datatypes because it doesnot get copy it gets orignal value from heap
+// { eamil: 'akshay@gmail.com' }
+// { eamil: 'akshay@gmail.com' }
