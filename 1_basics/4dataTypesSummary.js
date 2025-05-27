56a537e8ad8d6d1876d11e8f574beabb9e31794c
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-05-27 05:51:16 +0000

    dataType Summary

---
+++ 1_basics/4dataTypesSummary.js
@@ -0,0 +1,29 @@
+// primitive datatypes
+
+// call by value all changes are made to their copies not to orignal value 
+// 7 types: String , Number, Boolean, null, undefined , symbol , BigInt
+
+// const id = Symbol('123');
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+// console.log(id == anotherId);
+
+//  reference (NON Primitive)
+// Array, Object and Functions 
+
+// const arr = [1,2,3,4,5];
+
+let myObject  = {
+    myName:"akshay",
+    myage:23
+}
+
+
+let myFunctionName = function () {
+// This is also a function
+}
+
+// function myFuctionName (){
+// This is also a function
+// }
