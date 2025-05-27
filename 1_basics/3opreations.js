67466ccc35d23854e2cf8432ad650e41cb3f064f
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-05-27 05:10:34 +0000

    comparision completed

--- 1_basics/3opreations.js
+++ 1_basics/3opreations.js
@@ -1,11 +1,20 @@
 // ###################### Opreations ###############
-let value = 2
-let updatedValue = -value;
-console.log(updatedValue);
+// let value = 2
+// let updatedValue = -value;
+// console.log(updatedValue);
 
-console.log(2+3);
-console.log(2-3);
-console.log(2*3);
-console.log(2**3);
-console.log(2/3);
-console.log(3%2);
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(3%2);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);
+
+// console.log( (3 + 4) * 5 -10);
+
---
+++ 1_basics/4_comparision.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1);
+console.log("2" === 2);
+
+//AVOID THES TYPES OF COMPARISION
+
+// console.log(null>0);
+// console.log(null>=0);// value comparision problem  true
+// console.log(null<0); 
+// console.log(null<=0);
+// console.log(null==0);
+// equality check and and comparision is different in java script
+//strict check ===
+
+
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined<0);
