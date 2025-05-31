73b46d268f63637e835e1ccdee9f25ec89497ce2
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-05-31 10:04:54 +0000

    string done

--- 1_basics/05_strings.js
+++ 1_basics/05_strings.js
@@ -8,16 +8,32 @@ let otherString = new String('akshay');   // making a string as an object
 
 // we can use index value to acess the characters of string directly 
 
-console.log(otherString[1]);
-console.log(otherString.__proto__);
+// console.log(otherString[1]);
+// console.log(otherString.__proto__);
 
-let gameName = new String('Olympics-Games');
+let gameName = new String('Olympics');
 
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(5));
-console.log(gameName.indexOf('m'));
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('m'));
 
 // other Methods Of string
 
+let anotherName = gameName.substring(2,4);
+// console.log(anotherName);
+let anotherName2 = gameName.slice(-7,4);
+console.log(anotherName2);
+
+let anotherString = "            akshay         ";
+console.log(anotherString.trim());
+
+let url = "www.gilkoto.shop";
+console.log(url.replace('www.','https://'));
+
+let urlName = "akshay";
+console.log(urlName.includes('sha'))  //returns true ,false
+
+let urlName2 = "akshay-chandel-ji";
+console.log(urlName2.split('-'));   //"returns array"
