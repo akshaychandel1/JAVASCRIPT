d6549ca5e73d801155dbb18f0db49eed30638a95
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-01 15:53:15 +0000

    updated

---
+++ 1_basics/07_Dates.js
@@ -0,0 +1,17 @@
+let t = new Date();
+
+// console.log(t);   
+/// We should turn it to String for better readable form
+// console.log(t.toString());
+// console.log(t.toDateString());   // it gives only date not time
+// console.log(t.toLocaleString('en-IN'))
+// console.log(t.toJSON());  
+
+// let myCreatedDate = new Date(2025,0,25);
+// console.log(myCreatedDate.toString());
+
+console.log(Math.floor(Date.now()/1000));   // turned time to seconds
+
+
+
+
