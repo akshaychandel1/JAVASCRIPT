779fd4c275d3420f97acf2e17fea9a6c7afffa13
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-02 11:46:09 +0000

    slice and splice

--- 2_basics/01_arraybasics.js
+++ 2_basics/01_arraybasics.js
@@ -5,5 +5,28 @@
 // console.log(arr2);
 
 let arr = [0,1,2,3,4,5];
-console.log(arr[0]);
+// console.log(arr[0]);
+// **********Methods of Array**********
+// arr.push(66);
+// arr.push(7);
+// arr.pop();
+// // console.log(arr);
 
+// arr.unshift(33);   // add element at first
+// arr.unshift(9);
+// arr.shift();    // remove from first
+// console.log(arr.indexOf(66));
+
+// console.log(arr.includes(66));
+
+// console.log(arr);
+
+
+const arr3 = arr.join();    // turns array to string
+console.log("A", arr3);
+
+// ********* Slice and Splice **************
+console.log(arr3.slice(0,4));
+
+console.log("B",arr3);
+// console.log(arr3.splice(0,4));   // returned removed array 
