95b3ba7bd104e6d3d00f7824b186bdceb30998ec
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-03 11:03:01 +0000

    function commited

---
+++ 3_basics/01_functions.js
@@ -0,0 +1,18 @@
+// function addTwoNums(num1,num2) {
+//     console.log(num1+num2);
+// }
+
+// addTwoNums(4,8);
+// function addTwoNums(num1,num2) {
+//     // let result = num1 +num2 ;
+//     // return result;
+//     return num1+num2;
+// }
+// let result = addTwoNums(1,5)
+// console.log(result);
+
+function userIsLogged(username) {
+    return `${username} just LoggedIn`;
+}
+
+console.log(userIsLogged("Akshay"));
