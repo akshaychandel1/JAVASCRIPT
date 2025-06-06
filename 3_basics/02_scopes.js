17767218917c8f0e9ac22d6920cd343b61123eb8
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-06 11:47:06 +0000

    scope modified

--- 3_basics/02_scopes.js
+++ 3_basics/02_scopes.js
@@ -7,4 +7,39 @@ if (true) {
 }
 // console.log(a); // block scope
 // console.log(b); // block scope
-console.log(c); // global scope it will overwritten
+// console.log(c); // global scope it will overwritten
+
+
+// closure property
+
+// function one (){
+//     let num = 1;
+//     function two(){
+//         let num2 = 2;
+//         add = num + num2;
+//         console.log(add);
+//         // console.log(num);
+//     }
+//     console.log(two());
+    
+//     // console.log(num2);//out of scope
+//     // console.log(num);
+// }
+// // console.log(add);
+// console.log(one());
+
+
+
+// **************** MINI HOISTING ****************
+
+console.log(addone(5));
+function addone(num){
+    return num + 1; 
+}
+//  function is called before it is craeated
+
+let add2 = function(num2) {
+    return num2 + 2;
+}
+console.log(add2(5));
+
