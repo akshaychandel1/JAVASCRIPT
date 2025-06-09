2118d54390923ccf872a6b7f488ce6457f566fdb
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-09 06:53:20 +0000

    arrow basics

--- 3_basics/03_Arrows.js
+++ 3_basics/03_Arrows.js
@@ -4,8 +4,36 @@ const user = {
 
     fun: function() {
         console.log(`${this.username} is loggedIn`);
+        console.log(this);
     }
 }
-user.fun();
-user.username = "sam";
-user.fun();
+// user.fun();
+// user.username = "sam";
+// user.fun();
+
+// console.log(this);  // yeah empty aayega qunki we are working inside node environment but it will return window inside browser
+
+// function one() {
+//     let username = "chandel";
+//     console.log(this.username);   // this keyword wont work in function this work with object not with the function
+//     }
+// console.log(one());
+
+
+// arrow function
+let varFun = (num1,num2)=>{
+    return num1+num2;
+}
+
+console.log(varFun(3,4));
+
+//implecet return function   no need to write return keyword
+// let add2 = (num1,num2) => num1 +num2;
+// no need to write return keyword
+let add2 = (num1,num2)=>(num1+num2)
+console.log(add2(3,6));
+
+// wrapping of the objectr in the curly brackets in fat arrow function
+
+let objwrraping =()=>({username:"Akshay Chandel"})
+console.log(objwrraping());
