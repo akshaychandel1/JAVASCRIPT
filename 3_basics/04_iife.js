6e5013f6f201214314eaf58fc1b48ceaa455f307
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-09 07:29:40 +0000

    modified

---
+++ 3_basics/04_iife.js
@@ -0,0 +1,8 @@
+// Immediately Invoked Function Expression
+// to remove the global scope pollution so that our this function will not get affected
+
+(function one() {
+    console.log(`DB CONNECTED`);
+}) (); //this two brackets are one for call and second for exectution always use semicolon ;
+
+(()=>{console.log(`DB Connected Two`)})()
