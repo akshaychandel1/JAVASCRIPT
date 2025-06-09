72159086000ce90fdfb527d064a90f8266383a96
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-09 10:02:15 +0000

    modified 10000

--- 3_basics/04_iife.js
+++ 3_basics/04_iife.js
@@ -2,7 +2,9 @@
 // to remove the global scope pollution so that our this function will not get affected
 
 (function one() {
+    //named iife immediately invoked function axpression
     console.log(`DB CONNECTED`);
 }) (); //this two brackets are one for call and second for exectution always use semicolon ;
 
-(()=>{console.log(`DB Connected Two`)})()
+(()=>{console.log(`DB Connected Two`)})();
+((name)=>console.log(name))("akshay");
