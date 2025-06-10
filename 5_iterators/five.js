e13539d4aea11c460b244789fe5293674037ccac
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-10 12:29:39 +0000

    commit

---
+++ 5_iterators/five.js
@@ -0,0 +1,16 @@
+const coding = ['js','cpp','py','java']
+
+// coding.forEach(function (item) {
+//     console.log(item);
+// })
+
+// coding.forEach((val)=>{
+//     console.log(val);
+    
+// })
+
+function printFun(item) {
+console.log(item);
+}
+
+coding.forEach(printFun)
--- 5_iterators/four.js
+++ 5_iterators/four.js
@@ -10,7 +10,19 @@ const myLan = {
 //     console.log(key);
 // }
 
+
+
+/////  FOR IN
 // FOR  IN LOOP for Getting the key and Values both for the Object
-for (const key in myLan) {
-    console.log(`${key} :- ${myLan[key]}`);
-} 
+// for (const key in myLan) {
+//     console.log(`${key} :- ${myLan[key]}`);
+//      // it gives us keys of object for value we need to give it inside the nameofobject[key]
+// } 
+
+const programming = ["js","cpp","py","java"]
+for (const key in programming) {
+    console.log(programming[key]);
+    // it gives us keys of array for value we need to give it inside the nameofarray[]
+}
+
+// ALso Remember Maps Are Not Itteratiable
