7ec478ba0a7d2946da22aecdd6361bc458ccb57a
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-10 10:39:42 +0000

    pushed

---
+++ 5_iterators/one.js
@@ -0,0 +1,23 @@
+// ********FOR LOOP**********
+// let arr = [1,3,4,5,6,7,8,9];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// Table
+// for (let i = 0; i <= 10; i++) {
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);        
+//     }    
+// }
+
+// break
+
+for (let i = 0; i <= 20; i++) {
+    if (i == 5) {
+        console.log("5 is Detected");
+        break
+    }
+    console.log(`value of i = ${i}`);
+}
