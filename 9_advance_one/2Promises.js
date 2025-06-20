7f0a14195a6f862efe72351c794e8a451b33f443
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-20 14:29:36 +0000

    sdfgh

--- 9_advance_one/2Promises.js
+++ 9_advance_one/2Promises.js
@@ -1,36 +1,50 @@
-console.log('1');
+// console.log('1');
 
-const promise1 = new Promise(function (resolve,reject) {
-    // Do asyn Tasks
-    //DB Calls, cryptography network
-    console.log(2);
-    resolve()
-    console.log('10');
+// const promise1 = new Promise(function (resolve,reject) {
+//     // Do asyn Tasks
+//     //DB Calls, cryptography network
+//     console.log(2);
+//     resolve()
+//     console.log('10');
     
     
-    setTimeout(function(){
-        console.log('Asyn Task Is Complete');
-        // resolve()
-        console.log('3');
-    },2000)    
-}) 
-console.log(4);
+//     setTimeout(function(){
+//         console.log('Asyn Task Is Complete');
+//         // resolve()
+//         console.log('3');
+//     },2000)    
+// }) 
+// console.log(4);
 
 
-promise1.then(function(){
-    console.log('5');
-    console.log('Promise Consumed');
-    console.log('6');
-})
-console.log('7');
+// promise1.then(function(){
+//     console.log('5');
+//     console.log('Promise Consumed');
+//     console.log('6');
+// })
+// console.log('7');
 
 
-new Promise(function (resolve,reject) {
-    setTimeout(function(){
-        console.log("Async Task 2");
-        resolve()
+// new Promise(function (resolve,reject) {
+//     setTimeout(function(){
+//         console.log("Async Task 2");
+//         resolve()
+//     },1000)
+// }).then(function() {
+//     console.log('Async Resolved 2');
+    
+// })
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function() {
+        resolve({username : 'Akshay', age : '23'})
     },1000)
-}).then(function() {
-    console.log('Async Resolved 2');
+})
+promiseThree.then(function(userData) {
+    console.log(userData);
     
+})
+
+const Promise4 = new Promise(function (resolve,reject) {
+    setTimeout((){},2000)
 })
