b55eeda6c1058fdd229ee73a253fcb164f477621
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-20 07:10:50 +0000

    ist Part

---
+++ 9_advance_one/2Promises.js
@@ -0,0 +1,36 @@
+console.log('1');
+
+const promise1 = new Promise(function (resolve,reject) {
+    // Do asyn Tasks
+    //DB Calls, cryptography network
+    console.log(2);
+    resolve()
+    console.log('10');
+    
+    
+    setTimeout(function(){
+        console.log('Asyn Task Is Complete');
+        // resolve()
+        console.log('3');
+    },2000)    
+}) 
+console.log(4);
+
+
+promise1.then(function(){
+    console.log('5');
+    console.log('Promise Consumed');
+    console.log('6');
+})
+console.log('7');
+
+
+new Promise(function (resolve,reject) {
+    setTimeout(function(){
+        console.log("Async Task 2");
+        resolve()
+    },1000)
+}).then(function() {
+    console.log('Async Resolved 2');
+    
+})
