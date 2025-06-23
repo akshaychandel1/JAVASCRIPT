abd31965a2276bc0639790aea578d95bf6d07afc
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-23 06:27:27 +0000

    promised

--- 9_advance_one/2Promises.js
+++ 9_advance_one/2Promises.js
@@ -35,16 +35,78 @@
     
 // })
 
-const promiseThree = new Promise(function(resolve,reject){
-    setTimeout(function() {
-        resolve({username : 'Akshay', age : '23'})
-    },1000)
-})
-promiseThree.then(function(userData) {
-    console.log(userData);
+// const promiseThree = new Promise(function(resolve,reject){
+//     setTimeout(function() {
+//         resolve({username : 'Akshay', age : '23'})
+//     },1000)
+// })
+// promiseThree.then(function(userData) {
+//     console.log(userData);
     
+// })
+
+// const Promise4 = new Promise(function (resolve,reject) {
+//     setTimeout(function(){
+//         let error = false
+//         if (!error) {
+//             resolve({username: "Akshay Chandel Ji", password:"123"})
+//         } else{
+//             reject('Error: Something went Wrong')
+//         }
+//     },2000)
+// })
+// Promise4.then((user)=>{
+//     console.log(user);
+//     return user.username
+// }).then((username)=>{               /// This is Called Promise Chaning by using .then
+//     console.log(username);
+// }).catch(function(error){
+//     console.log(error);
+// }).finally(()=>{
+//     console.log("promise is finally resolved or rejected");
+// })
+
+
+const Promise5 = new Promise(function (resolve,reject) {
+    let error = false
+    if (!error) {
+        resolve({username:"JS",password:"123"})
+    } else{
+        reject('Error in Js')
+    }
 })
 
-const Promise4 = new Promise(function (resolve,reject) {
-    setTimeout((){},2000)
-})
+// async function consumePromise() {   ///without try catch
+//     let response = await Promise5
+//     console.log(response);
+// }
+async function consumePromise() {
+    try{
+        let response = await Promise5
+        console.log(response);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+consumePromise();
+
+
+async function getAllUsers() {
+ try {
+       const response = await fetch('https://api.github.com/users/akshaychandel1')
+    const user = await response.json()
+    console.log(user);
+}
+     catch (error) {
+    console.log("E:", error);
+ }
+}
+getAllUsers()
+
+fetch('https://api.github.com/users/akshaychandel1')
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+}).catch((error)=>console.log(error))
