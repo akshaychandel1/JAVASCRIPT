7c6845c5f63d8f4ecb6bbbf9284f144dd3f3c6c7
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-25 15:38:13 +0000

    allah hu akbar

---
+++ 10_oops/oop.js
@@ -0,0 +1,14 @@
+// const user = {
+//     username: "Akshay",
+//     signedIN: true,
+//     getAllUsers: function(){
+//         // console.log('Got user details from database');
+//         // console.log(this.username);
+//         console.log(this);
+        
+//     }
+// }
+// console.log(user.getAllUsers());
+
+
+
