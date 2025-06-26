0e2836b14dd1a12954e14743f81f7f3954192866
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-26 05:48:13 +0000

    commit

--- 10_oops/oop.js
+++ 10_oops/oop.js
@@ -5,10 +5,17 @@
 //         // console.log('Got user details from database');
 //         // console.log(this.username);
 //         console.log(this);
-        
 //     }
 // }
 // console.log(user.getAllUsers());
 
 
 
+function User(username,isLoggedIn){
+    this.username = username;
+    this.isLoggedIn = isLoggedIn;
+}
+const UserOne = new User("Akshay",12)
+const UserTwo = new User("Chandel",21)
+console.log(UserOne.constructor);
+// console.log(UserTwo);
