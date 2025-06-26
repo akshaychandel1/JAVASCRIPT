e278f485a9d05cd87d71a78ae267f53bf0537388
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2025-06-26 09:03:49 +0000

    commited

---
+++ 10_oops/inheritence.js
@@ -0,0 +1,23 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`User Name is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+    addCourse(){
+        console.log(`A Course Was Added By ${this.username}`);
+        
+    }
+}
+const newUser = Teacher("Kireedula Naresh", "naresh@3112@gmail.com", "123456");
+console.log(newUser);
+ 
